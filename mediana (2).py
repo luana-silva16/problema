f765d1486e6e92dc904fa3749b792ff72aa036d2
luana-silva16 <wagnertectec@gmail.com>
2024-11-01 01:12:20 -0300

    Add files via upload

---
+++ mediana (2).py
@@ -0,0 +1,13 @@
+5,7,9,12,50
+entrada = input()
+
+numeros_str = ["5", "7", "9", "12", "50"]
+lista = [float(x.strip()) for x in entrada.split(',')]
+
+soma = sum(lista)
+
+quantidade = len(lista)
+
+media = soma
+
+print(f'{media:.1f}')
